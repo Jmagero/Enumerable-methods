d115677ebaf586f5e4885f8dfa939e224ba4963d
jmagero <jmagero2017@gmail.com>
2020-05-29 17:50:39 +0300

    Added my_each method

---
+++ main.rb
@@ -0,0 +1,29 @@
+
+module Enumerable
+  def my_each
+    return enum_for unless  block_given?
+    my_arr = *self
+    i = 0 
+    while i < my_arr.length
+      yield my_arr[i]
+      i+=1
+    end
+
+  end
+
+  # def my_each_with_index 
+  #   return unless block_given?
+  #   for i in 0...self.length
+  #     yield self[i], i
+  #   end
+  # end
+
+  # def my_select
+  #   return unless block_given?
+  #   result = []
+  #   self.my_each{ |x| result << x if yield(x)}
+  #   result
+  # end
+end
+
+{"name" => "Celyn", "car" => "green"}.my_each{|elem| puts elem}
