c1046464d4a56b0337c5a7b407aedb41f0f9fe59
jmagero <jmagero2017@gmail.com>
2020-05-29 18:19:05 +0300

    Adds my_select

--- main.rb
+++ main.rb
@@ -20,12 +20,12 @@ module Enumerable
       end
   end
 
-  # def my_select
-  #   return unless block_given?
-  #   result = []
-  #   self.my_each{ |x| result << x if yield(x)}
-  #   result
-  # end
+  def my_select
+    return enum_for unless block_given?
+    result = []
+    self.my_each{ |x| result << x if yield(x)}
+    result
+  end
 end
 
 {"name" => "Celyn", "car" => "green"}.my_each{|elem| puts elem}
